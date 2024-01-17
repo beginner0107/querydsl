542f0c6cb428f133ac34ec329d84a76f5a7f42e8
Beginner <dkstmdwn17@naver.com>
2024-01-17 22:34:30 +0900

    프로젝션과 결과 반환 - 기본
    되도록이면 Tuple은 Repository Layer에서만 쓰자.

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -494,4 +494,30 @@ public class QuerydslBasicTest {
     // 참고: member.age.stringValue() 부분이 중요한데, 문자가 아닌 다른 타입들은 stringValue()로 문자로
     // 변환할 수 있다. 이 방법은 ENUM을 처리할 때도 자주 사용한다.
   }
+
+  @Test
+  public void simpleProjection() {
+    List<String> result = queryFactory
+        .select(member.username)
+        .from(member)
+        .fetch();
+
+    for (String s : result) {
+      System.out.println("s = " + s);
+    }
+  }
+
+  @Test
+  public void tupleProjection() {
+    List<Tuple> result = queryFactory
+        .select(member.username, member.age)
+        .from(member)
+        .fetch();
+    for (Tuple tuple : result) {
+      String username = tuple.get(member.username);
+      Integer age = tuple.get(member.age);
+      System.out.println("username = " + username);
+      System.out.println("age = " + age);
+    }
+  }
 }
