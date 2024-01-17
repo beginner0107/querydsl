230fd29f33a25b435110e2e2106e2b16903e495f
Beginner <dkstmdwn17@naver.com>
2024-01-17 23:10:37 +0900

    QueryProjection 활용 *주의*
    Querydsl에 의존적, 도입할 때 고민

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -580,6 +581,18 @@ public class QuerydslBasicTest {
 
     for (UserDto userDto : result) {
       System.out.println("userDto = " + userDto);
+    }
+  }
+
+  @Test
+  public void findDtoByQueryProjection() {
+    List<MemberDto> result = queryFactory
+        .select(new QMemberDto(member.username, member.age))
+        .from(member)
+        .fetch();
+    
+    for (MemberDto memberDto : result) {
+      System.out.println("result = " + result);
     }
   }
 }
