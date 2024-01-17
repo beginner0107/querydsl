d68481008502bf2db87bf7b643c1c92885cc91d4
Beginner <dkstmdwn17@naver.com>
2024-01-17 22:47:15 +0900

    JPQL DTO 프로젝션

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.dto.MemberDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
 import study.querydsl.entity.QTeam;
@@ -518,6 +519,18 @@ public class QuerydslBasicTest {
       Integer age = tuple.get(member.age);
       System.out.println("username = " + username);
       System.out.println("age = " + age);
+    }
+  }
+
+  @Test
+  public void findDtoByJPQL() {
+    List<MemberDto> result = em.createQuery
+            ("select new study.querydsl.dto.MemberDto(m.username, m.age) from Member m",
+                MemberDto.class)
+        .getResultList();
+
+    for (MemberDto memberDto : result) {
+      System.out.println("memberDto = " + memberDto);
     }
   }
 }
